27d22604e298d2f0fbd166db66ca361b14463389
mbootland <matthew.bootland@gmail.com>
2017-07-07 17:27:19 +0900

    expanding functionality

--- server.js
+++ server.js
@@ -54,3 +54,24 @@ var server = http.createServer(function(request, response){
 server.listen(3000, function(){
   console.log("Server listening on port 3000")
 });
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
+exports.listen = function(server) {
+  io = socketio.listen(server);
+  io.set('log level', 1);
+  io.sockets.on('connection', function (socket) {
+    guestNumber = assignGuestName(socket, guestNumber,
+      nickNames, namesUsed);
+    joinRoom(socket, 'Lobby');
+    handleMessageBroadcasting(socket, nickNames);
+    handleNameChangeAttempts(socket, nickNames, namesUsed);
+    handleRoomJoining(socket);
+    socket.on('rooms', function() {
+      socket.emit('rooms', io.sockets.manager.rooms);
+    });
+
+    handleClientDisconnection(socket, nickNames, namesUsed);
+  });
+};
