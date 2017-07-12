2125ff39dbc69b38a9df64751823b5dc9a39d881
mbootland <matthew.bootland@gmail.com>
2017-07-12 19:00:53 +0900

    no errors but app is not really working. debug tomorrow

--- server.js
+++ server.js
@@ -55,21 +55,3 @@ server.listen(3000, function(){
 
 var chatServer = require('./lib/chat_server');
 chatServer.listen(server);
-
-exports.listen = function(server) {
-  io = socketio.listen(server);
-  io.set('log level', 1);
-  io.sockets.on('connection', function (socket) {
-    guestNumber = assignGuestName(socket, guestNumber,
-      nickNames, namesUsed);
-    joinRoom(socket, 'Lobby');
-    handleMessageBroadcasting(socket, nickNames);
-    handleNameChangeAttempts(socket, nickNames, namesUsed);
-    handleRoomJoining(socket);
-    socket.on('rooms', function() {
-      socket.emit('rooms', io.sockets.manager.rooms);
-    });
-
-    handleClientDisconnection(socket, nickNames, namesUsed);
-  });
-};
